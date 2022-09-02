1f5aa67a903178950aaf849e04fcb4c1717567cf
prabhakardixith <prabhakar.dixith96@gmail.com>
2022-09-02 17:13:23 +0530

    reset

--- src/crud/Post.js
+++ src/crud/Post.js
@@ -14,6 +14,7 @@ const Post = ({setCount,edit,setEdit}) => {
           console.log(user);
           axios.post('https://pd-organic.herokuapp.com/user',user)
           .then(res => setCount(0))
+          .then(res => setUser({}))
           .catch(er => console.log("Error in Post : "+er.message))
         }
         else{
@@ -34,6 +35,7 @@ const Post = ({setCount,edit,setEdit}) => {
         <label>User Name : </label><input value={editor ? editor.userName : user.userName} type="text" onChange={(e)=> editor ? setEditor({...editor,userName:e.target.value}) : setUser({...user,userName:e.target.value})}/><br/>
         <label>User Email : </label><input value={editor ? editor.emailId : user.emailId} type="text" onChange={(e)=> editor ? setEditor({...editor,emailId:e.target.value}) : setUser({...user,emailId:e.target.value})}/><br/>
         <input type="submit"/>
+        <input type="reset" onClick={()=> editor ? setEditor({}) : setUser({})}/>
       </form>
     </div>
   )
